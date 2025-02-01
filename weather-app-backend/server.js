c5b2b7bdd75a16b150d1e0a3eff7c8baaa898447
joaobalca <joaomiguelbalca@gmail.com>
2025-02-01 17:31:13 +0000

    setup api deploy

--- weather-app-backend/server.js
+++ weather-app-backend/server.js
@@ -13,14 +13,10 @@ connectDB();
 
 app.use('/api', weatherRoutes);
 
-app.use((req, res, next) => {
-  console.log(`Received request: ${req.method} ${req.url}`);
-  next();
-});
 
 // Uncomment the code below to run the server locally
-// const PORT = process.env.PORT || 3001;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   
 module.exports = app;
   
